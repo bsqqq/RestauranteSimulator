2d4bd46067dc0fefba54253e4e01afccee5634bb
Vinicius Aragão <55242418+bsqqq@users.noreply.github.com>
2021-03-21 19:25:03 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -7,14 +7,14 @@ public class Gerente extends Funcionario{
 	
 	public String Identificacao(double index) throws InputMismatchException {
 		index = id % 9;
-		 nome[0] = "João Paulo";
-		 nome[1] = "Jorge Luíz";
+		 nome[0] = "Vinicius Moura AragÃ£o";
+		 nome[1] = "Guilherme Junqueira";
 		 nome[2] = "Lucas De Moura";
-		 nome[3] = "Arthur Cavalcante";
-		 nome[4] = "Vinicius Studart, O Major";
-		 nome[5] = "você mesmo (um clone de uma realidade paralela)";
-		 nome[6] = "o desenvolvedor deste programa";
-		 nome[7] = "Getúlio Vargas";
+		 nome[3] = "Felipe Deschamps";
+		 nome[4] = "Rafael Sousa";
+		 nome[5] = "vocÃª mesmo (um clone de uma realidade paralela)";
+		 nome[6] = "Gabriel Pato";
+		 nome[7] = "Julio Della Flora";
 		 nome[8] = "uma entidade superior";
 		 nome[9] = "Daniel Chagas";
 		 return nome[(int) id];
